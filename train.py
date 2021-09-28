40ffb6e05a8fea61c41f34a3386646f57a3591d9
taeukkkim <ktu9682@gmail.com>
2021-09-28 13:06:00 +0000

    baseline microF1 47.840

--- train.py
+++ train.py
@@ -7,9 +7,16 @@ import sklearn
 import numpy as np
 from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score
 from transformers import AutoTokenizer, AutoConfig, AutoModelForSequenceClassification, Trainer, TrainingArguments, RobertaConfig, RobertaTokenizer, RobertaForSequenceClassification, BertTokenizer
+from transformers.configuration_utils import PretrainedConfig
 from load_data import *
-from datasets import Dataset, DatasetDict, Features, Value, ClassLabel
+from datasets import Dataset, DatasetDict, Features, Value, ClassLabel, load_dataset
 
+from collator import DataCollator
+from preprocessing import mark_entity_spans as _mark_entity_spans
+from preprocessing import convert_example_to_features as _convert_example_to_features
+
+from functools import partial
+from typing import Tuple, List, Any, Dict
 
 # set fixed random seed
 def seed_everything(seed):
@@ -21,6 +28,13 @@ def seed_everything(seed):
     np.random.seed(seed)
     random.seed(seed)
 
+def softmax(arr: np.ndarray, axis: int = -1):
+    c = arr.max(axis=axis, keepdims=True)
+    s = arr - c
+    nominator = np.exp(s)
+    denominator = nominator.sum(axis=axis, keepdims=True)
+    probs = nominator / denominator
+    return probs
 
 def klue_re_micro_f1(preds, labels):
     """KLUE-RE micro f1 (except no_relation)"""
@@ -79,44 +93,95 @@ def label_to_num(label):
   return num_label
 
 def train():
+
+  wandb_project = "klue_re_tunib_koelectra_2"
+  report_to = "wandb"
+
   # load model and tokenizer
   # MODEL_NAME = "bert-base-uncased"
   # MODEL_NAME = "klue/bert-base"
-  seed_everything(args.seed)
+  seed_everything(42)
   MODEL_NAME = "tunib/electra-ko-small"
 
-  wandb_project = "klue_re_tunib_koelectra"
-  report_to = "wandb"
+    # Settings for Relation Extraction Baseline
+    # <subj>entity</subj> ~~ <obj>entity</obj> ~~
+  markers = dict(
+      subject_start_marker="<subj>",
+      subject_end_marker="</subj>",
+      object_start_marker="<obj>",
+      object_end_marker="</obj>",
+  )
+
+  relation_class = ['no_relation', 'org:top_members/employees', 'org:members',
+       'org:product', 'per:title', 'org:alternate_names',
+       'per:employee_of', 'org:place_of_headquarters', 'per:product',
+       'org:number_of_employees/members', 'per:children',
+       'per:place_of_residence', 'per:alternate_names',
+       'per:other_family', 'per:colleagues', 'per:origin', 'per:siblings',
+       'per:spouse', 'org:founded', 'org:political/religious_affiliation',
+       'org:member_of', 'per:parents', 'org:dissolved',
+       'per:schools_attended', 'per:date_of_death', 'per:date_of_birth',
+       'per:place_of_birth', 'per:place_of_death', 'org:founded_by',
+       'per:religion']
+
+  num_labels = len(relation_class)
+
+  id2label = {idx: label for idx, label in enumerate(relation_class)}
+  label2id = {label: idx for idx, label in enumerate(relation_class)}
+  
+  # Load Tokenizer
   tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+  tokenizer.add_special_tokens(
+        {"additional_special_tokens": list(markers.values())}
+  )
 
   # load dataset
-  train_dataset = load_data("../dataset/train/train.csv")
-  dev_dataset = load_data("../dataset/train/dev.csv") # validation용 데이터는 따로 만드셔야 합니다.
+  train_data = load_dataset("jinmang2/load_klue_re", script_version="v1.0.1b", split='train')
+  valid_data = load_dataset("jinmang2/load_klue_re", script_version="v1.0.1b", split='valid')
 
-  train_label = label_to_num(train_dataset['label'].values)
-  dev_label = label_to_num(dev_dataset['label'].values)
+  # Preprocess and tokenizing
+  mark_entity_spans = partial(_mark_entity_spans, **markers)
+  convert_example_to_features = partial(
+      _convert_example_to_features,
+      tokenizer=tokenizer,
+      **markers,
+  )
 
-  # tokenizing dataset
-  tokenized_train = tokenized_dataset(train_dataset, tokenizer)
-  tokenized_dev = tokenized_dataset(dev_dataset, tokenizer)
+  train_examples = train_data.map(mark_entity_spans)
+  tokenized_train_datasets = train_examples.map(convert_example_to_features)
+  valid_examples = valid_data.map(mark_entity_spans)
+  tokenized_valid_datasets = valid_examples.map(convert_example_to_features)
 
   # make dataset for pytorch.
-  RE_train_dataset = RE_Dataset(tokenized_train, train_label)
-  RE_dev_dataset = RE_Dataset(tokenized_dev, dev_label)
+  # RE_train_dataset = RE_Dataset(tokenized_train, train_label)
+  # RE_dev_dataset = RE_Dataset(tokenized_dev, dev_label)
   
   device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
   print(device)
   # setting model hyperparameter
-  model_config =  AutoConfig.from_pretrained(MODEL_NAME)
-  model_config.num_labels = 30
+  model_config = AutoConfig.from_pretrained(
+        pretrained_model_name_or_path=MODEL_NAME,
+        num_labels=num_labels,
+        cache_dir="cache",
+        id2label=id2label,
+        label2id=label2id,
+    )
+  # model_config =  AutoConfig.from_pretrained(MODEL_NAME)
+  # model_config.num_labels = 30
 
-  model =  AutoModelForSequenceClassification.from_pretrained(MODEL_NAME, config=model_config)
+  model =  AutoModelForSequenceClassification.from_pretrained(MODEL_NAME, config=model_config, cache_dir="cache")
   print(model.config)
   model.parameters
   model.to(device)
   
+  if model.config.vocab_size < len(tokenizer):
+    print("resize...")
+    model.resize_token_embeddings(len(tokenizer))
 
+  data_collator = DataCollator(tokenizer)
+
+  # Set-up WANDB
   os.environ["WANDB_PROJECT"] = wandb_project
 
   call_wandb = True
@@ -133,7 +198,7 @@ def train():
   # 사용한 option 외에도 다양한 option들이 있습니다.
   # https://huggingface.co/transformers/main_classes/trainer.html#trainingarguments 참고해주세요.
   training_args = TrainingArguments(
-    output_dir='./results',          # output directory
+    output_dir='./results/0928_tunib_electra',          # output directory
     save_total_limit=5,              # number of total save model.
     save_steps=500,                 # model saving step.
     num_train_epochs=10,              # total number of training epochs
@@ -142,9 +207,10 @@ def train():
     per_device_eval_batch_size=32,   # batch size for evaluation
     warmup_steps = 500,               # number of warmup steps for learning rate scheduler
     weight_decay=0.01,               # strength of weight decay
-    logging_dir='./logs',            # directory for storing logs
+    logging_dir='./0928_logs',            # directory for storing logs
     logging_steps=100,              # log saving step.
     report_to=report_to,
+    run_name = "baseline-0928-taeukkkim",
     evaluation_strategy='steps', # evaluation strategy to adopt during training
                                 # `no`: No evaluation during training.
                                 # `steps`: Evaluate every `eval_steps`.
@@ -152,17 +218,68 @@ def train():
     eval_steps = 500,            # evaluation step.
     load_best_model_at_end = True 
   )
+
+  # remove unused feature names
+  features_name = list(tokenized_train_datasets.features.keys())
+  features_name.pop(features_name.index("input_ids"))
+  features_name.pop(features_name.index("label"))
+  tokenized_train_datasets = tokenized_train_datasets.remove_columns(features_name)
+  tokenized_valid_datasets = tokenized_valid_datasets.remove_columns(features_name)
+
   trainer = Trainer(
     model=model,                         # the instantiated 🤗 Transformers model to be trained
     args=training_args,                  # training arguments, defined above
-    train_dataset=RE_train_dataset,         # training dataset
-    eval_dataset=RE_dev_dataset,             # evaluation dataset
+    train_dataset=tokenized_train_datasets,         # training dataset
+    eval_dataset=tokenized_valid_datasets,             # evaluation dataset
+    data_collator = data_collator,
     compute_metrics=compute_metrics         # define metrics function
   )
 
   # train model
   trainer.train()
-  model.save_pretrained('./best_model')
+  model.save_pretrained('./0928_best_model')
+
+  # Load Checkpoint
+  config = PretrainedConfig.from_json_file(os.path.join('./0928_best_model', "config.json"))
+  # We load the model state dict on the CPU to avoid an OOM error.
+  state_dict = torch.load(os.path.join('./0928_best_model', "pytorch_model.bin"), map_location="cpu")
+  # If the model is on the GPU, it still works!
+  trainer._load_state_dict_in_model(state_dict)
+  del state_dict
+
+  # print(list(trainer.model.parameters())[-1])
+
+  # Inference
+  test_data = load_dataset("jinmang2/load_klue_re", script_version="v1.0.1b", split='test')
+  test_id = test_data["guid"]
+  examples = test_data.map(mark_entity_spans)
+  tokenized_test_datasets = examples.map(convert_example_to_features)
+
+  features_name = list(tokenized_test_datasets.features.keys())
+  features_name.pop(features_name.index("input_ids"))
+
+  tokenized_test_datasets = tokenized_test_datasets.remove_columns(features_name)
+
+  logits = trainer.predict(tokenized_test_datasets)[0]
+  probs = softmax(logits).tolist()
+  result = np.argmax(logits, axis=-1).tolist()
+  pred_answer = [id2label[v] for v in result]
+
+  ## make csv file with predicted answer
+  #########################################################
+  # 아래 directory와 columns의 형태는 지켜주시기 바랍니다.
+  output = pd.DataFrame(
+      {
+          'id':test_id,
+          'pred_label':pred_answer,
+          'probs':probs,
+      }
+  )
+  output.to_csv(f'./prediction/submission_0928_tunib_electra_small.csv', index=False)
+  #### 필수!! ##############################################
+  print('---- Finish! ----')
+
+
 def main():
   train()
 
