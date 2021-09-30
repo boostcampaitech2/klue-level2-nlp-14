336edd7157c337f6915eb1403f0f10be89a11672
taeukkkim <ktu9682@gmail.com>
2021-09-30 07:24:23 +0000

    baseline microF1 56.503

--- sh/run_electra.sh
+++ sh/run_electra.sh
@@ -1,13 +1,13 @@
 python train.py \
          --epochs 10 \
          --model_name tunib/electra-ko-en-base \
-         --lr 5e-5 \
+         --lr 3e-5 \
          --batch_size 32 \
          --valid_batch_size 32 \
          --warmup_ratio 0.2 \
-         --output_name 0929_tunib_koen_electra_base \
+         --output_name 0929_tunib_koen_electra_base_3e5 \
          --wandb_project klue_re_tunib_koelectra_taeuk \
-         --run_name 0929_tunib_koen_electra_base \
-         --output_dir 0929_tunib_koen_electra_base \
-         --best_model_dir 0929_tunib_koen_electra_base \
+         --run_name 0929_tunib_koen_electra_base_3e5 \
+         --output_dir 0929_tunib_koen_electra_base_3e5 \
+         --best_model_dir 0929_tunib_koen_electra_base_3e5 \
          
