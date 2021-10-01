a3678e95ab98b7215006f1516e9571cd47b77bf7
HaramLee <twinha0204@gmail.com>
2021-10-01 04:47:29 +0000

    fix some errors and change variable names

--- solution/utils/metrics.py
+++ solution/utils/metrics.py
@@ -16,20 +16,21 @@ def compute_micro_f1(logits, labels, label_indices=None):
     return micro_f1
 
     
-def compute_auprc(probs, labels):
+def compute_auprc(logits, labels):
     """ Compute Area Under the Precision-Recall Curve """
     onehots = np.eye(N_CLASSES)[labels]
     scores = np.zeros((N_CLASSES,))
     for c in range(N_CLASSES):
         targets_c = onehots.take([c], axis=1).ravel()
-        prods_c = probs.take([c], axis=1).ravel()
+        prods_c = logits.take([c], axis=1).ravel()
         precision, recall, _ = precision_recall_curve(targets_c, prods_c)
         scores[c] = auc(recall, precision)
     auprc = np.average(scores)
     return auprc
     
 
-def get_confusion_matrix(labels, preds):
+def get_confusion_matrix(logits, labels):
+    preds = np.argmax(logits, axis=1).ravel()
     cm = confusion_matrix(labels, preds)
     norm_cm = cm / np.sum(cm, axis=1)[:,None]
     cm = pd.DataFrame(norm_cm, index=RELATION_CLASS, columns=RELATION_CLASS)
@@ -40,17 +41,17 @@ def get_confusion_matrix(labels, preds):
     
 def compute_klue_re_leaderboard(eval_pred):
     # Parsing predictions and labels
-    preds, labels = eval_pred
+    logits, labels = eval_pred
     # Preprocess no_relation for micro F1
     no_relation_label_idx = RELATION_CLASS.index("no_relation")
     label_indices = list(range(len(RELATION_CLASS)))
     label_indices.remove(no_relation_label_idx)
     # Compute micro F1
-    micro_f1 = compute_micro_f1(preds, labels, label_indices)
+    micro_f1 = compute_micro_f1(logits, labels, label_indices)
     # Compute AURPC
-    auprc = compute_auprc(preds, labels)
+    auprc = compute_auprc(logits, labels)
     # Compute confusion matrix
-    cm_fig = get_confusion_matrix(labels, preds)
+    cm_fig = get_confusion_matrix(logits, labels)
     wandb.log({'confusion matrix': wandb.Image(cm_fig)})
     return {
         "micro_f1": micro_f1,
